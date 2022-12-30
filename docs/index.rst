21ea73f4398a24ae0e8ae082e373ee035aa9eaa5
DanielMA <danoan2008@gmail.com>
2022-12-30 20:40:27 +0100

    First commit

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. quick-notes documentation master file, created by
+   sphinx-quickstart on Sat Dec 31 12:29:51 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to quick-notes's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   reference/modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ test/test_parsers.py
@@ -0,0 +1,32 @@
+from danoan.quick_notes.commands import to_toml, to_markdown
+from danoan.quick_notes.control import model
+
+from test.conftest import *
+import pytest
+
+
+@pytest.mark.parametrize("n_entries", [1, 2, 5])
+def test_valid_markdown(n_entries, tmp_path):
+    list_of_mock_quick_note = [MockQuickNoteFactory.next() for _ in range(n_entries)]
+    expected_quick_note_table = model.QuickNoteTable(
+        [x.quick_note for x in list_of_mock_quick_note]
+    )
+
+    quick_note_table = to_toml.parse(
+        "\n".join([x.markdown_string() for x in list_of_mock_quick_note])
+    )
+    assert quick_note_table == expected_quick_note_table
+
+
+@pytest.mark.parametrize("n_entries", [1, 2, 5])
+def test_valid_quick_note_table(n_entries, tmp_path):
+    list_of_mock_quick_note = [MockQuickNoteFactory.next() for _ in range(n_entries)]
+    expected_quick_note_markdown = "\n".join([x.markdown_string() for x in list_of_mock_quick_note])
+    expected_quick_note_markdown += (
+        "\n"  # This accounts for the newline added by the template render
+    )
+
+    quick_note_table = model.QuickNoteTable([x.quick_note for x in list_of_mock_quick_note])
+    quick_note_markdown = to_markdown.parse(quick_note_table)
+
+    assert quick_note_markdown == expected_quick_note_markdown
