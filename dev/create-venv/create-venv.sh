79966bcfafc53f4b391702127ae6d9ac504e9e2b
Daniel Martins Antunes <danoan2008@gmail.com>
2023-09-06 23:18:46 +0200

    Update dev scripts

--- dev/create-venv/create-venv.sh
+++ dev/create-venv/create-venv.sh
@@ -1,19 +1,23 @@
 #! /usr/bin/env bash
 
-SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_PATH="${SCRIPT_PATH%quick-notes*}quick-notes"
 
 INPUT_FOLDER="${SCRIPT_PATH}/input"
 OUTPUT_FOLDER="${SCRIPT_PATH}/output"
 mkdir -p "${OUTPUT_FOLDER}"
 
-pushd "${PROJECT_PATH}" > /dev/null
+set -e
 
+pushd "${PROJECT_PATH}" >/dev/null
+
+trap "echo 'Aborted!'" err
 python3.8 -m venv .venv
+
 source .venv/bin/activate
 
 pip install --upgrade pip
-pip install -e . 
+pip install -e .
+pip install tox
 
-popd > /dev/null
-
+popd >/dev/null
